d6114d7a5d0cadd7f8cb4a38037a45aeaec339bc
rngnnn <rngnnnclk@gmail.com>
2024-08-19 13:08:44 +0200

    add file Switch

--- src/components/FlexandResponsive.jsx
+++ src/components/FlexandResponsive.jsx
@@ -29,6 +29,15 @@ const FlexandResponsive = () => {
         <div className="bg-red-500 ">5</div>
         <div className="bg-red-600 ">6</div>
       </div>
+      <h1>Config</h1>
+      <div className="bg-primary">
+        <div className="bg-blue-950">1</div>
+        <div className="">2</div>
+        <div className=" ">3</div>
+        <div className=" ">4</div>
+        <div className="">5</div>
+        <div className=" ">6</div>
+      </div>
     </div>
   );
 }
--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -2,35 +2,54 @@ import React from 'react'
 
 const Typography = () => {
   return (
+    //Fragment
+    // <>
+    // <React.Fragment key=""> key proeprtysi kullanmamız lazım ama doma da yük bindirmek istemiyorsak Fragment componentini kullnabaliriz. 
     <div>
-        <h3 className='text-2xl' >Typohraphy</h3>
-        <p className='text-sm' >Typohraphy</p>
-        <p className='text-lg' >Typohraphy</p>
-        <p className='text-xl' >Typohraphy</p>
-        <p className='text-3xl' >Typohraphy</p>
-        <p className='text-4xl' >Typohraphy</p>
-        <p className='text-6xl' >Typohraphy</p>
-        <p className='text-[75px]' >Typohraphy</p>
-        <article>
-            <p className='line-clamp- hover:line-clamp-none' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis consectetur aut illo soluta adipisci eligendi ipsa enim expedita! Quisquam incidunt eum aliquid architecto est voluptate veritatis iusto doloribus cumque id! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum tempora ex nemo reiciendis voluptatibus sapiente impedit magnam nesciunt earum praesentium. Quis maxime eveniet perferendis laborum totam architecto ipsa tenetur ex.
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem a, totam quam maxime quos esse consectetur eaque perspiciatis iusto, nulla, voluptatibus hic in maiores sequi ab ad saepe vero dolores.
-Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, cumque assumenda. Iste itaque velit asperiores sunt nam autem maxime. Architecto aperiam dolores cumque accusantium animi, reiciendis molestias adipisci. Atque, numquam.
+      <h3 className="text-2xl">Typohraphy</h3>
+      <p className="text-sm">Typohraphy</p>
+      <p className="text-lg">Typohraphy</p>
+      <p className="text-xl">Typohraphy</p>
+      <p className="text-3xl">Typohraphy</p>
+      <p className="text-4xl">Typohraphy</p>
+      <p className="text-6xl">Typohraphy</p>
+      <p className="text-[75px]">Typohraphy</p>
+      <p className="text-[10rem]">Typohraphy</p>
+      <article>
+        <p className="line-clamp-4 hover:line-clamp-none">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+          nesciunt excepturi iusto alias aperiam optio eveniet. Et laboriosam
+          voluptatibus quisquam ducimus, incidunt praesentium, tempora sed esse
+          quidem minima aut, fugit laborum? Voluptate nulla natus cupiditate rem
+          maiores commodi obcaecati voluptatum cum numquam delectus corporis,
+          officia consequatur et fugiat aliquid modi molestiae nam. Esse a
+          itaque tempora deleniti ipsam alias quo ducimus culpa doloremque
+          consequuntur cumque quaerat optio consequatur quam explicabo tenetur
+          excepturi aut, ipsa beatae animi laboriosam nisi. Exercitationem
+          veritatis, facilis obcaecati, quam est cupiditate pariatur minus ad
+          explicabo voluptatibus nesciunt? Eveniet delectus provident voluptates
+          cupiditate beatae. Maxime natus aperiam atque vero eos voluptates,
+          repellendus veniam, sit ipsa sapiente ut earum magni expedita
+          molestias voluptatem sint quisquam nam dolorum unde ab? Voluptas quas,
+          quae perferendis hic veniam dolorem culpa praesentium, totam itaque
+          ducimus sint inventore, rem dolor doloribus perspiciatis tempore nulla
+          enim vitae dicta? Repellendus placeat tenetur obcaecati excepturi
+          magnam?
+        </p>
+      </article>
 
-
-            </p>
-        </article>
-
-
-        <p className='text-center' >Typohraphy</p>
-        <p className='underline' >Typohraphy</p>
-        <p className='line-through' >Typohraphy</p>
-        <p className='text-red-300' >Typohraphy</p>
-        <p className='text-green-500' >Typohraphy</p>
-        <p className='text-6xl text-[rgb(154,200,205)]' >Typohraphy</p>
-        <p className='text-[75px]' >Typohraphy</p>
-
+      <p className="text-center">Typohraphy</p>
+      <p className="underline">Typohraphy</p>
+      <p className="line-through">Typohraphy</p>
+      <p className="text-red-300">Typohraphy</p>
+      <p className="text-green-500">Typohraphy</p>
+      <p className="text-6xl text-[rgb(154,200,205)]">Typohraphy</p>
+      <p className="text-[#1E0A32]">Typohraphy</p>
+      <p className="bg-[#b5a5c5] text-white">Typohraphy</p>
     </div>
-  )
+
+// </React.Fragment>
+  );
 }
 
 export default Typography
